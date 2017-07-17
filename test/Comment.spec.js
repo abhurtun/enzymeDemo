25a0a8e0f5948c84198cf69c24a3355b66e94d17
Arvin Bhurtun <arvin.bhurtun@spotlight.com>
2017-07-17 12:44:28 +0100

    very quick demo of how to use enzyme

---
+++ test/Comment.spec.js
@@ -0,0 +1,26 @@
+describe('a passing test', () => {
+  it('should pass', () => {
+    expect(true).to.be.true;
+  });
+});
+
+import React from 'react';
+import Comment from '../src/components/Comment';
+
+describe('Comment item', () => {
+  const wrapper = shallow(<Comment />);
+
+  it('should be a list item', () => {
+    expect(wrapper.type()).to.eql('li');
+  });
+
+  it('renders the custom comment text', () => {
+  wrapper.setProps({ comment: 'sympathetic ink' });
+  expect(wrapper.contains('sympathetic ink')).to.equal(true);
+  });
+
+  it('has a class name of "comment-item"', () => {
+    expect(wrapper.find('.comment-item')).to.have.length(1);
+  });
+
+});
---
+++ test/CommentsList.spec.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CommentList from '../src/components/CommentsList';
+
+it('calls componentDidMount', () => {
+	spy(CommentList.prototype, 'componentDidMount');
+
+    const wrapper = mount(<CommentList />);
+    expect(CommentList.prototype.componentDidMount.calledOnce).to.equal(true);
+});
+
+it('reverses the comments on the button click', () => {
+  const wrapper = mount(<CommentList />);
+
+  wrapper.setState({ comments: ['hello'] });
+  wrapper.find('button').simulate('click');
+  //for debugging
+  //console.log(wrapper.state());
+  expect(wrapper.state().comments[0].to.equal('olleh'))
+});
